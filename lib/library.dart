b634fe33bfc4fdd1f2cbdb1c6fc622edff729092
TrueCollaboration <truecollaboration0@gmail.com>
2023-10-04 17:58:43 +0200

    0.0.3: fix

--- lib/library.dart
+++ lib/library.dart
@@ -4,3 +4,4 @@ export 'src/external/interfaces/IBloc.dart';
 export 'src/external/interfaces/IBlocEvent.dart';
 export 'src/external/interfaces/IRepository.dart';
 export 'src/external/base_bloc.dart';
+export 'src/external/base_repository.dart';
