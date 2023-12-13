124c735c23acc39f61e21074451273f2d6a525f7
TrueCollaboration <pshonka.dev@gmail.com>
2023-12-13 02:10:45 +0100

    ...

--- lib/library.dart
+++ lib/library.dart
@@ -4,6 +4,8 @@ export 'package:bloc_common/library.dart' show
   IBlocEvent,
   IRepository,
   BaseRepository;
+  
+export 'package:bloc_common/src/external/events.dart';
 
 export 'src/external/interfaces/IBloc.dart';
 export 'src/external/base_bloc.dart';
